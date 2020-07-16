c768241671a210278bcf117779d1e63c5b6562d2
Sanskaar Patni <patnisanskaar@gmail.com>
2020-07-16 22:37:33 +0530

    Cleaned up Code deployed to Heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const config = require('config');
-const router = express.Router();
+const router = require('./router');
 const isAuth = require('./middleware/isauth');
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
@@ -10,16 +10,13 @@ const graphQlResolvers = require('./graphql/resolvers/index');
 const app = express();
 
 app.use(isAuth);
+app.use(router);
 
 app.use('/graphql', graphqlHTTP({
     schema: graphQlSchema,
     rootValue: graphQlResolvers,
     graphiql: true
 }));
-
-router.get("/", (req, res) => {
-    res.send("Server is up and running");
-});
 
 mongoose
     .connect(config.get('MONGODB_URI'), {
@@ -30,14 +27,6 @@ mongoose
     .then(() => console.log('MongoDB Connected!'))
     .catch(err => console.log(err));
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Set a static folder
-//     app.use(express.static('client/build'));
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-
-// }
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
